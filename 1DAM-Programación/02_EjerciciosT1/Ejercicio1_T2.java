093523f2953654b34c5fa85f7cab6457b97e7b8e
Iván <114758527+ICasGar@users.noreply.github.com>
2022-11-08 16:28:10 +0100

    Add files via upload

---
+++ 1DAM-Programación/02_EjerciciosT1/Ejercicio1_T2.java
@@ -0,0 +1,18 @@
+package pkg01_ejerciciost2;
+public class Ejercicio1_T2 {
+    public static void main(String[] args) {
+        String movil;
+        System.out.println("Ordenes:");
+        System.out.println("Enciendete");
+        System.out.println("Apagate");
+        System.out.println("Saca fotos");
+        System.out.println("Preguntas: ");
+        System.out.println("Cual es la resolucion de la camara?");
+        System.out.println("5000px");
+        System.out.println("Cuanta capacidad tienes?");
+        System.out.println("108GB");
+        System.out.println("Tiene Wifi?");
+        System.out.println("Si");
+    }
+    
+}
