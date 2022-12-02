c1ee1275d84fc864ddb3276dcc575687d2916b35
Iván <114758527+ICasGar@users.noreply.github.com>
2022-12-02 17:15:46 +0100

    Add files via upload

---
+++ 1DAM-Programación/02_EjerciciosT1/JavaApplication46.java
@@ -0,0 +1,10 @@
+package javaapplication46;
+import bpc.daw.mario.*; 
+import bpc.daw.mario.Luigi;
+public class JavaApplication46 {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
