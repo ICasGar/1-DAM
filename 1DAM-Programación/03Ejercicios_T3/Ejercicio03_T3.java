3e208b5fbee440a8eef7d3cd3415aff0566d9d1c
Iván <114758527+ICasGar@users.noreply.github.com>
2023-01-13 17:48:19 +0100

    Update Ejercicio03_T3.java

--- 1DAM-Programación/03Ejercicios_T3/Ejercicio03_T3.java
+++ 1DAM-Programación/03Ejercicios_T3/Ejercicio03_T3.java
@@ -4,8 +4,9 @@ public class Ejercicio03_T3 {
     public static void main(String[] args) {
         boolean going=true;
         int num=0;
+        int numero;
         while(going){
-            int numero=(int)(Math.random()*10+1);
+            numero=(int)(Math.random()*10+1);
             System.out.println(numero);
             if(numero==10){
                 going=false;
